c7f7964390eca8a5c78bc48f7f7a6cb170b6aae6
lewis-1991 <lewis-1991@hotmail.com>
2023-02-09 21:45:09 -0500

    Fusión del menú en desktop

---
+++ java.js
@@ -0,0 +1,12 @@
+const menuemail = document.querySelector(".navbar-email");
+const desktopmenu = document.querySelector(".desktop-menu");
+
+
+menuemail.addEventListener("click",toggledesktopmenu);
+
+function toggledesktopmenu(){
+
+    desktopmenu.classList.toggle("inactive");
+}
+
+console.log("hola");
