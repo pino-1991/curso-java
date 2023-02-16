09adf42a493b7a1ccbc8ec84274126d3a7fa51c0
lewis-1991 <lewis-1991@hotmail.com>
2023-02-15 20:07:38 -0500

    version 1

--- java.js
+++ java.js
@@ -1,12 +1,28 @@
 const menuemail = document.querySelector(".navbar-email");
 const desktopmenu = document.querySelector(".desktop-menu");
+const menuhambicono = document.querySelector(".menu");
+const mobilemenu = document.querySelector(".mobile-menu");
+const menucarrito = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
+
+
 
 
 menuemail.addEventListener("click",toggledesktopmenu);
+menuhambicono.addEventListener("click",togglemobilemenu);
+menucarrito.addEventListener("click",togglecarrito);
+
 
 function toggledesktopmenu(){
 
     desktopmenu.classList.toggle("inactive");
 }
 
-console.log("hola");
+function togglemobilemenu(){
+
+    mobilemenu.classList.toggle("inactive");
+}
+function togglecarrito(){
+
+    aside.classList.toggle("inactive");
+}
